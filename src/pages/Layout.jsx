4605f03d6bf50bc997ec1940d31edd8ab00cec94
jessesillman <jesse.sillman@yahoo.com>
2024-02-18 19:25:09 +0200

    adjusted top menu bar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,25 +1,28 @@
 import { Outlet, Link } from "react-router-dom";
-import { Nav, Navbar } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 const Layout = () => {
   return (
     <>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand as={Link} to="/">My Website</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container> {/* Wrap the Navbar content in a Container */}
+          <Navbar.Brand as={Link} to="/">My Website</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
       
-      <Outlet />
+      <Container className="mt-3"> {/* Add Container here if you want to wrap Outlet contents */}
+        <Outlet />
+      </Container>
     </>
   );
 };
 
 export default Layout;
-
